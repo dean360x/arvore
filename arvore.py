0a2d1cc56aead80be3133739a76acca9a771bf26
Van.br <mundoads360@gmail.com>
2024-09-24 15:07:30 -0300

    Salvando arvore.py

--- arvore.py
+++ arvore.py
@@ -1,8 +1,10 @@
+
+#SISTEMA DE ARQUIVOS COM ÁRVORE.
 class No:
     def __init__(self, nome, eh_diretorio=False):
         self.nome = nome
         self.eh_diretorio = eh_diretorio
-        self.filhos = []  # Lista de subdiretórios ou arquivos
+        self.filhos = []  #Lista para subdiretórios ou arquivos.
 
     def adicionar_filho(self, filho):
         self.filhos.append(filho)
@@ -13,7 +15,7 @@ class No:
         for filho in self.filhos:
             filho.exibir(nivel + 1)
 
-# Criando o sistema de arquivos
+#Cria o sistema de arquivos.
 raiz = No("raiz", True)
 casa = No("casa", True)
 usuario = No("usuario", True)
@@ -22,7 +24,7 @@ arquivo1 = No("arquivo1.txt")
 arquivo2 = No("arquivo2.txt")
 arquivo3 = No("relatorio.docx")
 
-# Estrutura hierárquica (árvore)
+#Estrutura hierárquica (árvore).
 raiz.adicionar_filho(casa)
 casa.adicionar_filho(usuario)
 usuario.adicionar_filho(documentos)
@@ -30,6 +32,7 @@ documentos.adicionar_filho(arquivo1)
 documentos.adicionar_filho(arquivo2)
 usuario.adicionar_filho(arquivo3)
 
-# Exibindo o sistema de arquivos
+
+#Exibindo o sistema de arquivos.
 print("Estrutura do Sistema de Arquivos:")
 raiz.exibir()
